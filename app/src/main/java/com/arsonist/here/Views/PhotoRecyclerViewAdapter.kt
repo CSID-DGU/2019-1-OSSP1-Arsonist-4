56d8f145627c1b2a0187781faeadeffefcc669d4
dge03078 <dge03078@gmail.com>
2019-05-13 19:36:37 +0900

    feature/develope fastScroll in RecyclerView

--- app/src/main/java/com/arsonist/here/Views/PhotoRecyclerViewAdapter.kt
+++ app/src/main/java/com/arsonist/here/Views/PhotoRecyclerViewAdapter.kt
@@ -1,19 +1,20 @@
 package com.arsonist.here.Views
 
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.arsonist.here.PhotoMetadata
 import com.arsonist.here.R
 import com.bumptech.glide.Glide
+import com.l4digital.fastscroll.FastScroller
 import kotlinx.android.synthetic.main.item_photo_view.view.*
 
-class PhotoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val photoView = itemView.photoView
 
-    fun loadView(photoMetadata: PhotoMetadata){
+    fun loadView(photoMetadata: PhotoMetadata) {
         Glide.with(itemView.context)
             .load(photoMetadata.data)
             .centerCrop()
@@ -22,7 +23,8 @@ class PhotoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
     }
 }
 
-class PhotoRecyclerViewAdapter: RecyclerView.Adapter<PhotoViewHolder>(){
+class PhotoRecyclerViewAdapter : RecyclerView.Adapter<PhotoViewHolder>(),
+    FastScroller.SectionIndexer {
 
     private val photoMetadataList = mutableListOf<PhotoMetadata>()
 
@@ -30,15 +32,22 @@ class PhotoRecyclerViewAdapter: RecyclerView.Adapter<PhotoViewHolder>(){
         photoMetadataList.add(photoMetadata)
         notifyItemInserted(photoMetadataList.size - 1)
     }
+
+    override fun getSectionText(position: Int): CharSequence {
+        return "$position"
+    }
+    // 설명 뜨는거 수정할 것
+    // override fun getSectionText(position: Int) = photoMetadataList[position].location.toString()
+
     override fun getItemCount() = photoMetadataList.size
 
     override fun onBindViewHolder(viewHolder: PhotoViewHolder, position: Int) {
-        photoMetadataList.getOrNull(position)?.let{
+        photoMetadataList.getOrNull(position)?.let {
             viewHolder.loadView(it)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-        return PhotoViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_photo_view, parent,false))
+        return PhotoViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_photo_view, parent, false))
     }
 }
